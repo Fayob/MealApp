a42de5737da62c9f733a6717aadce1ab91575207
fayob <adedejifavour6@gmail.com>
2023-12-06 10:12:46 +0100

    Update meal detail screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,30 +1,78 @@
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { useContext, useLayoutEffect } from 'react';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { useSelector, useDispatch } from "react-redux";
+
 import { MEALS } from '../data/dummy_data';
 import MealDetails from '../components/MealDetails';
+import Subtitle from '../components/MealDetail/Subtitle';
+import List from '../components/MealDetail/List';
+import IconButton from '../components/IconButton';
+import { addFavorite, removeFavorite } from '../store/redux/favorites';
+// import { FavoritesContext } from '../store/context/favorite-context';
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({ route, navigation }) {
+  // const favoriteMealContext = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids)
+  const dispatch = useDispatch()
+
   const mealId = route.params.mealId;
 
   const selectMeal = MEALS.find((meal) => meal.id === mealId);
+
+  const mealIsFavorite = favoriteMealIds.includes(mealId);
+
+  function changeFavoriteStatusHandler() {
+    if (mealIsFavorite) {
+      // favoriteMealContext.removeFavorites(mealId)
+      dispatch(removeFavorite({ id: mealId }))
+    } else {
+      // favoriteMealContext.addFavorites(mealId)
+      dispatch(addFavorite({ id: mealId }))
+    }
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon={mealIsFavorite ? "star" : "star-outline"}
+            color="white"
+            onPress={changeFavoriteStatusHandler}
+          />
+        );
+      },
+    });
+  }, [navigation, changeFavoriteStatusHandler]);
+
   return (
-    <ScrollView style={styles.container}>
+    <ScrollView style={styles.rootContainer}>
       <Image source={{ uri: selectMeal.imageUrl }} style={styles.image} />
-      <Text style={[styles.text, { fontWeight: 'bold'}]}> {selectMeal.title} </Text>
+      <Text style={[styles.title, { fontWeight: 'bold' }]}>
+        {selectMeal.title}
+      </Text>
       <View>
         <MealDetails
           duration={selectMeal.duration}
           complexity={selectMeal.complexity}
           affordability={selectMeal.affordability}
+          textStyle={styles.detailText}
         />
       </View>
-      <Text style={styles.text}>Ingredient </Text>
-      {selectMeal.ingredients.map((ingredient) => (
-        <Text key={ingredient}>{ingredient}</Text>
-      ))}
-      <Text style={styles.text}>Steps: {selectMeal.steps}</Text>
-      {selectMeal.steps.map((step) => (
-        <Text key={step}>{step}</Text>
-      ))}
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredient</Subtitle>
+          <List data={selectMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectMeal.steps} />
+        </View>
+      </View>
     </ScrollView>
   );
 }
@@ -32,15 +80,27 @@ function MealDetailScreen({ route }) {
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    margin: 16,
-    backgroundColor: 'white',
+  rootContainer: {
+    marginBottom: 32,
   },
   image: {
     width: '100%',
     height: 400,
   },
-  text: {
-    margin: 8
-  }
-})
+  title: {
+    color: 'white',
+    margin: 8,
+    fontWeight: 'bold',
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  detailText: {
+    color: 'white',
+  },
+  listContainer: {
+    width: '80%',
+  },
+  listOuterContainer: {
+    alignItems: 'center',
+  },
+});
